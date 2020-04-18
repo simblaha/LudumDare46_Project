cd058f9254f16a91f6896ead05f958787536d91f
Croped <lhaemmerle@gmx.at>
2020-04-19 00:07:37 +0200

    Update PlayerMovement.cs
    add groundcheck to prevent player from jumping midair

--- LudumDare46/Assets/Scripts/PlayerMovement.cs
+++ LudumDare46/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public int jumpForce;
+    public LayerMask groundLayer;
 
     private Vector2 move;
     private Rigidbody2D rb;
@@ -19,10 +20,21 @@ public class PlayerMovement : MonoBehaviour
     {
         move = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rb.velocity.y);
         rb.velocity = move;
-
         if (Input.GetButtonDown("Jump"))
         {
-            rb.AddForce(Vector2.up * jumpForce);
+            if (GroundCheck())
+            {
+                rb.AddForce(Vector2.up * jumpForce);
+            }
         }
+    }
+
+    bool GroundCheck()
+    {
+        if (Physics2D.Raycast(transform.position, Vector2.down, GetComponent<Collider2D>().bounds.extents.y + 0.01f, groundLayer))
+        {
+            return true;
+        }
+        return false;
     }
 }
